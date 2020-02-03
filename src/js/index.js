c702fcc01975dfef0d51eaeb624eb631096ad357
Alex Dingal <lhexie2014@gmail.com>
2020-02-03 22:56:04 +0800

    Add comment on index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import * as date_functions from '../../../helper_functions/date';
 
+// Automate copyright year
 const indexYear = document.getElementById('year');
 date_functions.insertDate(indexYear);
